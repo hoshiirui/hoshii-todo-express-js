8242eb432a61aef6028c81618d6d85da22e1b768
Rangga Permana <kotomaru7969@gmail.com>
2023-07-28 09:17:41 +0800

    frontend completed for filter

--- frontend/src/components/TodoList.js
+++ frontend/src/components/TodoList.js
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from "axios"
 import { Link } from 'react-router-dom'
-import { useNavigate, useParams } from 'react-router-dom'
+// import { useNavigate, useParams } from 'react-router-dom'
 
 const TodoList = () => {
     //a little internal css
+    const buttonSpace = {
+      marginRight: '1rem',
+    };
+
     const customStyle = {
       color: 'green',
     };
@@ -12,17 +16,23 @@ const TodoList = () => {
     // For Getting Data
     const[todos, setTodos] = useState([])
     const[orderMode, setOrderMode] = useState("id")
+    const[filterMode, setFilterMode] = useState(3)
 
     useEffect(() => {
       sortTodo()
-    }, [orderMode]);
+    }, [orderMode, filterMode]);
 
     const orderChange = (e) => {
       setOrderMode(e.target.value)
     }
 
+    const filterChange = (e) => {
+      setFilterMode(e.target.value)
+      // console.log(filterMode)
+    }
+
     const sortTodo = async () =>{
-      const response = await axios.get(`http://localhost:5000/todos/order/${orderMode}`)
+      const response = await axios.get(`http://localhost:5000/todos/${orderMode}/${filterMode}`)
       setTodos(response.data)
     }
 
@@ -58,6 +68,7 @@ const TodoList = () => {
               <div className="card" style={{borderRadius: '15px'}}>
                 <div className="card-body p-5">
                   <h4 className="mb-3">Awesome Todo List</h4>
+
                   <div className="form-group mb-3">
                     <label htmlFor="orderMode" className="form-label">Order By:</label>
                     <select className="form-control" id="orderMode" value={orderMode} onChange={orderChange}>
@@ -66,6 +77,16 @@ const TodoList = () => {
                       <option value="title">Title</option>
                     </select>
                   </div>
+                  <div className="form-group mb-3">
+                    <label htmlFor="filterMode" className="form-label">Filter By:</label>
+                    <select className="form-control" id="filterMode" value={filterMode} onChange={filterChange}>
+                      <option value='3'>All</option>
+                      <option value='0'>To-do</option>
+                      <option value='1'>In Progress</option>
+                      <option value='2'>Done</option>
+                    </select>
+                  </div>
+
                   {/* Menampilkan isinya */}
                   <ul className="list-group mb-0">
                   <h6><b>Due Today</b></h6>
@@ -82,7 +103,8 @@ const TodoList = () => {
                           return (
                             <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-0">
                             <div className="d-flex align-items-center">
-                                <input className="form-check-input me-2" type="checkbox" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)} defaultValue aria-label="..."/>
+                                {/* <input className="form-check-input me-2" type="checkbox" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)} defaultValue aria-label="..."/> */}
+                                <button style={buttonSpace} className="btn btn-primary" type="button" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)}>To-Do</button>
                                 <b>{todo.title}</b>: {todo.description}
                             </div>
                             <p>{deadlineTime}</p> {/* Display the extracted time */}
@@ -96,7 +118,8 @@ const TodoList = () => {
                           return (
                             <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-0">
                             <div className="d-flex align-items-center">
-                                <input className="form-check-input me-2" type="checkbox" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)} defaultValue aria-label="..." defaultChecked />
+                                {/* <input className="form-check-input me-2" type="checkbox" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)} defaultValue aria-label="..." defaultChecked /> */}
+                                <button style={buttonSpace} className="btn btn-success" type="button" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)}>Done</button>
                                 <s><b>{todo.title}</b>: {todo.description}</s>
                             </div>
                             <p>{deadlineTime}</p> {/* Display the extracted time */}
@@ -110,7 +133,8 @@ const TodoList = () => {
                           return (
                             <li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-0">
                             <div className="d-flex align-items-center">
-                                <input className="form-check-input me-2" type="checkbox" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)} defaultValue aria-label="..." />
+                                {/* <input className="form-check-input me-2" type="checkbox" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)} defaultValue aria-label="..." /> */}
+                                <button style={buttonSpace} className="btn btn-warning" type="button" onClick={()=> changeStatus(todo.id, todo.title, todo.description, todo.status, todo.deadline, todo.userid)}>In Progress</button>
                                 <span style={customStyle}><b>{todo.title}</b>: {todo.description}</span>
                             </div>
                             <p>{deadlineTime}</p> {/* Display the extracted time */}
